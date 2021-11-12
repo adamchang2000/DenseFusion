fixed rotation rep bug, should be good now

datasets/ycb/dataset.py
@@ -8,7 +8,7 @@ import torchvision.transforms as transforms
 import argparse
 import time
 import random
-from lib.transformations import quaternion_from_euler, euler_matrix, random_quaternion, quaternion_matrix, rotation_matrix_from_vectors_procedure, axis_angle_of_rotation_matrix
+from lib.transformations import quaternion_from_euler, euler_matrix, random_quaternion, quaternion_matrix, rotation_matrix_from_vectors_procedure, axis_angle_of_rotation_matrix, rotation_matrix_of_axis_angle
 import numpy.ma as ma
 import copy
 import scipy.misc
@@ -305,9 +305,19 @@ class PoseDataset(data.Dataset):
         #axis will be the same as R_around_front
         axis, angle = axis_angle_of_rotation_matrix(R_around_front)
 
+        #negate the axis and the angle
+        if np.abs(np.linalg.norm(axis + front_r)) < 0.001:
+            axis = -axis
+            angle = -angle
+
+        if angle < 0:
+            angle += np.pi * 2
+        if angle > np.pi * 2:
+            angle -= np.pi * 2
+
         assert (angle >= 0 and angle <= np.pi * 2)
         
-        angle_bin = int(angle / self.num_rot_bins)
+        angle_bin = int(angle / 2 / np.pi * self.num_rot_bins)
 
         #calculate other peaks based on size of symm
         if len(symm) > 0:
@@ -382,6 +392,8 @@ class PoseDataset(data.Dataset):
         depth = '{0}/{1}-depth.png'.format(self.root, self.list[index])
         label = '{0}/{1}-label.png'.format(self.root, self.list[index])
         meta = '{0}/{1}-meta.mat'.format(self.root, self.list[index])
+
+        print("loading sample", self.list[index])
 
         if color in self.preloaded_img:
             img = self.preloaded_img[color]
@@ -477,8 +489,16 @@ class PoseDataset(data.Dataset):
 
         rot_bins = np.zeros(self.num_rot_bins).astype(np.float32)
 
+        print("FRONT 1", front)
+
         #find rotation matrix that goes from front -> front_r
         Rf = rotation_matrix_from_vectors_procedure(front, front_r)
+
+        print("original calculated rf")
+        print(Rf)
+
+        print("front_r from gt", front_r)
+        print("Rf @ front", Rf @ front)
 
         #find residual rotation
         R_around_front = target_r @ Rf.T
@@ -486,9 +506,31 @@ class PoseDataset(data.Dataset):
         #axis will be the same as R_around_front
         axis, angle = axis_angle_of_rotation_matrix(R_around_front)
 
+        #negate the axis and the angle
+        if np.abs(np.linalg.norm(axis + front_r)) < 0.001:
+            print("axis flipped")
+            axis = -axis
+            angle = -angle
+
+        print("these should be the same")
+        print(front_r, axis)
+
+        print("test here, target_r, then our calculated")
+
+        R_around_front_recovered = rotation_matrix_of_axis_angle(axis, angle)
+
+        print(target_r)
+        print(R_around_front @ Rf)
+        print(R_around_front_recovered @ Rf)
+
+        if angle < 0:
+            angle += np.pi * 2
+        if angle > np.pi * 2:
+            angle -= np.pi * 2
+
         assert (angle >= 0 and angle <= np.pi * 2)
         
-        angle_bin = int(angle / self.num_rot_bins)
+        angle_bin = int(angle / 2 / np.pi * self.num_rot_bins)
 
         #calculate other peaks based on size of symm
         if len(symm) > 0:
@@ -544,9 +586,11 @@ class PoseDataset(data.Dataset):
         
         return cloud.astype(np.float32), \
                choose.astype(np.int32), \
-               self.norm(img_masked.astype(np.float32)), \
+               self.norm(torch.from_numpy(img_masked.astype(np.float32))).numpy(), \
                front_r.astype(np.float32), \
                rot_bins.astype(np.float32), \
+               angle.astype(np.float32), \
+               Rf.astype(np.float32), \
                front.astype(np.float32), \
                target_r.astype(np.float32), \
                target_t.astype(np.float32), \
@@ -609,13 +653,6 @@ def get_bbox(label):
         cmax = img_length
         cmin -= delt
     return rmin, rmax, cmin, cmax
-
-if __name__ == "__main__":
-    dataset = PoseDataset('test', 1000, False, './datasets/ycb/YCB_Video_Dataset', 0, False, 36)
-
-    data = dataset.get_item_debug(0)
-    cloud, choose, img_masked, front_r, rot_bins, front, target_r, target_t, model_points, idx = data
-
-    print(target_r)
+    
 
 

lib/loss.py
@@ -11,6 +11,10 @@ from lib.transformations import rotation_matrix_from_vectors, rotation_matrix_of
 
 cross_entropy_loss = nn.CrossEntropyLoss(reduction='none')
 
+rot_bins_loss_coeff = 1
+front_loss_coeff = 1
+translation_loss_coeff = 4
+
 def loss_calculation(pred_front, pred_rot_bins, pred_t, pred_c, front_r, rot_bins, front_orig, t, idx, model_points, points, w, refine, num_rot_bins):
 
     bs, num_p, _ = pred_c.size()
@@ -31,7 +35,7 @@ def loss_calculation(pred_front, pred_rot_bins, pred_t, pred_c, front_r, rot_bin
     #pred_t loss (L2 norm on translation)
     pred_t_loss = torch.norm(((pred_t + points) - t), dim=2).unsqueeze(-1)
 
-    loss = torch.mean((pred_front_dis + pred_rot_loss + pred_t_loss) * pred_c - w * torch.log(pred_c))
+    loss = torch.mean((pred_front_dis * front_loss_coeff + pred_rot_loss * rot_bins_loss_coeff + pred_t_loss * translation_loss_coeff) * pred_c - w * torch.log(pred_c))
 
     pred_t = pred_t.contiguous().view(bs * num_p, 1, 3)
 

lib/loss_refiner.py
@@ -11,6 +11,10 @@ from lib.transformations import rotation_matrix_from_vectors, rotation_matrix_of
 
 cross_entropy_loss = nn.CrossEntropyLoss(reduction='none')
 
+rot_bins_loss_coeff = 1
+front_loss_coeff = 1
+translation_loss_coeff = 4
+
 def loss_calculation(pred_front, pred_rot_bins, pred_t, front_r, rot_bins, front_orig, t, idx, points, num_rot_bins):
     
     pred_front = pred_front.view((1, 1, 3))
@@ -36,7 +40,7 @@ def loss_calculation(pred_front, pred_rot_bins, pred_t, front_r, rot_bins, front
     #pred_t loss (L2 norm on translation)
     pred_t_loss = torch.norm((pred_t - t), dim=2).unsqueeze(-1)
 
-    loss = torch.mean(pred_front_dis + pred_rot_loss + pred_t_loss)
+    loss = torch.mean(pred_front_dis * front_loss_coeff + pred_rot_loss * rot_bins_loss_coeff + pred_t_loss * translation_loss_coeff)
 
     #calculating new model_points for refiner
     #requires finding highest confidence front and theta and solving for rotation matrix

tools/visualize_ycb.py
@@ -26,25 +26,20 @@ from lib.network import PoseNet, PoseRefineNet
 from lib.loss import Loss
 from lib.loss_refiner import Loss_refine
 
-from lib.transformations import rotation_matrix_from_vectors, rotation_matrix_of_axis_angle
+from lib.transformations import rotation_matrix_from_vectors_procedure, rotation_matrix_of_axis_angle
 
 import open3d as o3d
 
 def visualize_points(model_points, front_orig, front, angle, t, label):
-
-    model_points = model_points.cpu().detach().numpy()
-    front = front.cpu().detach().numpy()
-    front_orig = front_orig.cpu().detach().numpy()
-    angle = angle.cpu().detach().numpy()
-    t = t.cpu().detach().numpy()
-
-    Rf = rotation_matrix_from_vectors(front_orig, front)
+    Rf = rotation_matrix_from_vectors_procedure(front_orig, front)
 
     R_axis = rotation_matrix_of_axis_angle(front, angle)
 
-    R_tot = (R_axis @ Rf).T
+    R_tot = (R_axis @ Rf)
 
-    pts = (model_points @ R_tot + t).squeeze()
+    print("our rot rep", R_tot)
+
+    pts = (model_points @ R_tot.T + t).squeeze()
 
     pcld = o3d.geometry.PointCloud()
     pts = o3d.utility.Vector3dVector(pts)
@@ -54,7 +49,7 @@ def visualize_points(model_points, front_orig, front, angle, t, label):
     o3d.io.write_point_cloud(label + ".ply", pcld)
 
 def visualize_pointcloud(points, label):
-    points = points.cpu().detach().numpy().reshape((-1, 3))
+    points = points.reshape((-1, 3))
 
     pcld = o3d.geometry.PointCloud()
     points = o3d.utility.Vector3dVector(points)
@@ -112,11 +107,6 @@ def main():
         raise Exception("this is visualizer code, pls pass in a model lol")
 
     if opt.dataset == 'ycb':
-        dataset = PoseDataset_ycb('train', opt.num_points, False, opt.dataset_root, 0.0, opt.refine_model != '', opt.num_rot_bins)
-    elif opt.dataset == 'linemod':
-        dataset = PoseDataset_linemod('train', opt.num_points, False, opt.dataset_root, 0.0, opt.refine_model != '', opt.num_rot_bins)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=opt.workers)
-    if opt.dataset == 'ycb':
         test_dataset = PoseDataset_ycb('test', opt.num_points, False, opt.dataset_root, 0.0, opt.refine_model != '', opt.num_rot_bins)
     elif opt.dataset == 'linemod':
         test_dataset = PoseDataset_linemod('test', opt.num_points, False, opt.dataset_root, 0.0, opt.refine_model != '', opt.num_rot_bins)
@@ -130,19 +120,12 @@ def main():
     criterion = Loss(opt.num_rot_bins)
     criterion_refine = Loss_refine(opt.num_rot_bins)
 
-    best_test = np.Inf
-
-    st_time = time.time()
-
-    train_count = 0
-    train_loss_avg = 0.0
-
     estimator.eval()
 
     if opt.refine_model != "":
         refiner.eval()
 
-    for i, data in enumerate(dataloader, 0):
+    for i, data in enumerate(testdataloader, 0):
         points, choose, img, front_r, rot_bins, front_orig, t, model_points, idx = data
         points, choose, img, front_r, rot_bins, front_orig, t, model_points, idx = Variable(points).cuda(), \
                                                             Variable(choose).cuda(), \
@@ -191,77 +174,6 @@ def main():
             print("finished visualizing!")
             exit()
 
-
-    # logger = setup_logger('epoch%d_test' % epoch, os.path.join(opt.log_dir, 'epoch_%d_test_log.txt' % epoch))
-    # logger.info('Test time {0}'.format(time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - st_time)) + ', ' + 'Testing started'))
-    # test_loss = 0.0
-    # test_count = 0
-    # estimator.eval()
-    # refiner.eval()
-
-    # for j, data in enumerate(testdataloader, 0):
-    #     points, choose, img, front_r, rot_bins, front_orig, t, model_points, idx = data
-    #     points, choose, img, front_r, rot_bins, front_orig, t, model_points, idx = Variable(points).cuda(), \
-    #                                                          Variable(choose).cuda(), \
-    #                                                          Variable(img).cuda(), \
-    #                                                          Variable(front_r).cuda(), \
-    #                                                          Variable(rot_bins).cuda(), \
-    #                                                          Variable(front_orig).cuda(), \
-    #                                                          Variable(t).cuda(), \
-    #                                                          Variable(model_points).cuda(), \
-    #                                                          Variable(idx).cuda()
-    #     pred_front, pred_rot_bins, pred_t, pred_c, emb = estimator(img, points, choose, idx)
-    #     loss, new_points, new_rot_bins, new_t = criterion(pred_front, pred_rot_bins, pred_t, pred_c, front_r, rot_bins, front_orig, t, idx, model_points, points, opt.w, opt.refine_start)
-            
-    #     if opt.refine_start:
-    #         for ite in range(0, opt.iteration):
-    #             pred_front, pred_rot_bins, pred_t = refiner(new_points, emb, idx)
-    #             loss, new_points, new_rot_bins, new_t = criterion_refine(pred_front, pred_rot_bins, pred_t, front_r, new_rot_bins, front_orig, new_t, idx, new_points)
-
-    #     test_loss += loss.item()
-    #     logger.info('Test time {0} Test Frame No.{1} loss:{2}'.format(time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - st_time)), test_count, loss))
-
-    #     test_count += 1
-
-    # test_loss = test_loss / test_count
-    # logger.info('Test time {0} Epoch {1} TEST FINISH Avg dis: {2}'.format(time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - st_time)), epoch, test_loss))
-    # if test_loss <= best_test:
-    #     best_test = test_loss
-    #     if opt.refine_start:
-    #         torch.save(refiner.state_dict(), '{0}/pose_refine_model_{1}_{2}.pth'.format(opt.outf, epoch, test_loss))
-    #     else:
-    #         torch.save(estimator.state_dict(), '{0}/pose_model_{1}_{2}.pth'.format(opt.outf, epoch, test_loss))
-    #     print(epoch, '>>>>>>>>----------BEST TEST MODEL SAVED---------<<<<<<<<')
-
-    # if best_test < opt.decay_margin and not opt.decay_start:
-    #     opt.decay_start = True
-    #     opt.lr *= opt.lr_rate
-    #     opt.w *= opt.w_rate
-    #     optimizer = optim.Adam(estimator.parameters(), lr=opt.lr)
-
-    # if best_test < opt.refine_margin and not opt.refine_start:
-    #     opt.refine_start = True
-    #     opt.batch_size = int(opt.batch_size / opt.iteration)
-    #     optimizer = optim.Adam(refiner.parameters(), lr=opt.lr)
-
-    #     if opt.dataset == 'ycb':
-    #         dataset = PoseDataset_ycb('train', opt.num_points, True, opt.dataset_root, opt.noise_trans, opt.refine_start, opt.num_rot_bins)
-    #     elif opt.dataset == 'linemod':
-    #         dataset = PoseDataset_linemod('train', opt.num_points, True, opt.dataset_root, opt.noise_trans, opt.refine_start, opt.num_rot_bins)
-    #     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=opt.workers)
-    #     if opt.dataset == 'ycb':
-    #         test_dataset = PoseDataset_ycb('test', opt.num_points, False, opt.dataset_root, 0.0, opt.refine_start, opt.num_rot_bins)
-    #     elif opt.dataset == 'linemod':
-    #         test_dataset = PoseDataset_linemod('test', opt.num_points, False, opt.dataset_root, 0.0, opt.refine_start, opt.num_rot_bins)
-    #     testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=opt.workers)
-        
-    #     opt.sym_list = dataset.get_sym_list()
-    #     opt.num_points_mesh = dataset.get_num_points_mesh()
-
-    #     print('>>>>>>>>----------Dataset loaded!---------<<<<<<<<\nlength of the training set: {0}\nlength of the testing set: {1}\nnumber of sample points on mesh: {2}\nsymmetry object list: {3}'.format(len(dataset), len(test_dataset), opt.num_points_mesh, opt.sym_list))
-
-    #     criterion = Loss(opt.num_points_mesh, opt.sym_list)
-    #     criterion_refine = Loss_refine(opt.num_points_mesh, opt.sym_list)
 
 if __name__ == '__main__':
     main()