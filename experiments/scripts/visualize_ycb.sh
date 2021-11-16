73028d6fea4da17049af1a9d71b221ec24c26fd9
Adam Chang <adamchang2000@berkeley.edu>
2021-11-16 13:10:04 -0800

    more stuff

--- experiments/scripts/visualize_ycb.sh
+++ experiments/scripts/visualize_ycb.sh
@@ -6,6 +6,5 @@ set -e
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
-python3 ./tools/visualize_ycb.py --dataset_root ./datasets/ycb/YCB_Video_Dataset\
-  --model pose_model_current.pth\
-  --refine_model pose_refine_model_current.pth
+python ./tools/visualize_ycb.py --dataset_root C:/Users/OpenARK/Desktop/datasets/YCB_Video_Dataset\
+  --model pose_model_current2.pth
