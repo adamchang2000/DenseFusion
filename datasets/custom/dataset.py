63d06a4b7db654823c7c6953b41f4cc503093d19
james60708 <james60708@gmail.com>
2021-11-28 19:22:55 +0800

    finish custom dataset

--- datasets/custom/dataset.py
+++ datasets/custom/dataset.py
@@ -19,6 +19,7 @@ import scipy.misc
 import scipy.io as scio
 import yaml
 import cv2
+import open3d as o3d
 
 
 class PoseDataset(data.Dataset):
@@ -26,130 +27,95 @@ class PoseDataset(data.Dataset):
         self.objlist = [1]
         self.mode = mode
 
-        self.list_rgb = []
-        self.list_depth = []
-        self.list_label = []
-        self.list_obj = []
-        self.list_rank = []
-        self.meta = {}
-        self.pt = {}
+        self.data_list = []
+        self.model_list = []
         self.root = root
         self.noise_trans = noise_trans
         self.refine = refine
+        self.object_number = []
 
-        item_count = 0
         for item in self.objlist:
-            path = "{0}/{1}".format(self.root, item)
-            print(path)
-            files = [f.split('.')[0] for f in os.listdir(path) if f.find("left.png") != -1]
-            print(files)
-
-            for file in files: 
-                self.list_rgb.append('{0}/{1}.left.png'.format(path, item, input_line))
-                self.list_depth.append('{0}/data/{1}/depth/{2}.png'.format(self.root, '%02d' % item, input_line))
-                if self.mode == 'eval':
-                    self.list_label.append('{0}/segnet_results/{1}_label/{2}_label.png'.format(self.root, '%02d' % item, input_line))
-                else:
-                    self.list_label.append('{0}/data/{1}/mask/{2}.png'.format(self.root, '%02d' % item, input_line))
-                
-                #self.list_obj.append(item)
-                #self.list_rank.append(int(input_line))
-
-            
-            '''
+                           
             if self.mode == 'train':
-                input_file = open('{0}/data/{1}/train.txt'.format(self.root, '%02d' % item))
+                input_file = open('{}/split/{}/train.txt'.format(self.root, item))
             else:
-                input_file = open('{0}/data/{1}/test.txt'.format(self.root, '%02d' % item))
+                input_file = open('{}/split/{}/test.txt'.format(self.root, item))
             while 1:
-                item_count += 1
                 input_line = input_file.readline()
-                if self.mode == 'test' and item_count % 10 != 0:
-                    continue
                 if not input_line:
                     break
                 if input_line[-1:] == '\n':
                     input_line = input_line[:-1]
-                self.list_rgb.append('{0}/data/{1}/rgb/{2}.png'.format(self.root, '%02d' % item, input_line))
-                self.list_depth.append('{0}/data/{1}/depth/{2}.png'.format(self.root, '%02d' % item, input_line))
-                if self.mode == 'eval':
-                    self.list_label.append('{0}/segnet_results/{1}_label/{2}_label.png'.format(self.root, '%02d' % item, input_line))
-                else:
-                    self.list_label.append('{0}/data/{1}/mask/{2}.png'.format(self.root, '%02d' % item, input_line))
-                
-                self.list_obj.append(item)
-                self.list_rank.append(int(input_line))
+                self.data_list.append('{}/data/{}/{}'.format(self.root, item, input_line))
+                self.object_number.append(item)
 
-            meta_file = open('{0}/data/{1}/gt.yml'.format(self.root, '%02d' % item), 'r')
-            self.meta[item] = yaml.load(meta_file)
-            self.pt[item] = ply_vtx('{0}/models/obj_{1}.ply'.format(self.root, '%02d' % item))
-            
             print("Object {0} buffer loaded".format(item))
-            '''
 
-        self.length = len(self.list_rgb)
+        self.length = len(self.data_list)
+        print("Dataset Length: {}".format(self.length))
 
-        self.cam_cx = 325.26110
-        self.cam_cy = 242.04899
-        self.cam_fx = 572.41140
-        self.cam_fy = 573.57043
+        with open("{}/data/{}/_camera_settings.json".format(self.root, self.objlist[0])) as cam_config:
+            cam_data = json.load(cam_config)
+            self.cam_cx = float(cam_data["camera_settings"][0]["intrinsic_settings"]["cx"])
+            self.cam_cy = float(cam_data["camera_settings"][0]["intrinsic_settings"]["cy"])
+            self.cam_fx = float(cam_data["camera_settings"][0]["intrinsic_settings"]["fx"])
+            self.cam_fy = float(cam_data["camera_settings"][0]["intrinsic_settings"]["fy"])
 
-        self.xmap = np.array([[j for i in range(640)] for j in range(480)])
-        self.ymap = np.array([[i for i in range(640)] for j in range(480)])
+        self.xmap = np.array([[j for i in range(1280)] for j in range(720)])
+        self.ymap = np.array([[i for i in range(1280)] for j in range(720)])
         
         self.num = num
         self.add_noise = add_noise
         self.trancolor = transforms.ColorJitter(0.2, 0.2, 0.2, 0.05)
         self.norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        self.border_list = [-1, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600, 640, 680]
         self.num_pt_mesh_large = 500
         self.num_pt_mesh_small = 500
-        self.symmetry_obj_idx = [7, 8]
+        self.symmetry_obj_idx = []
 
     def __getitem__(self, index):
-        img = Image.open(self.list_rgb[index])
-        ori_img = np.array(img)
-        depth = np.array(Image.open(self.list_depth[index]))
-        label = np.array(Image.open(self.list_label[index]))
-        obj = self.list_obj[index]
-        rank = self.list_rank[index]        
+        path_prefix = self.data_list[index]
+        object_id = self.object_number[index]
+        model_path = "{}/models/{}/1_centered.obj".format(self.root, object_id)
 
-        if obj == 2:
-            for i in range(0, len(self.meta[obj][rank])):
-                if self.meta[obj][rank][i]['obj_id'] == 2:
-                    meta = self.meta[obj][rank][i]
-                    break
-        else:
-            meta = self.meta[obj][rank][0]
+        img = Image.open(path_prefix + ".left.png")
+
+        depth = np.array(Image.open(path_prefix + ".left.depth.16.png"))
+        label = np.array(Image.open(path_prefix + ".left.cs.png"))
+        with open(path_prefix + ".left.json") as cam_config:
+            meta = json.load(cam_config)
+
+        with open("{}/data/{}/_object_settings.json".format(self.root, object_id)) as object_config:
+            object_data = json.load(object_config)
+            object_class_id = object_data["exported_objects"][0]["segmentation_class_id"]
 
         mask_depth = ma.getmaskarray(ma.masked_not_equal(depth, 0))
+        mask_label = ma.getmaskarray(ma.masked_equal(label, np.array(object_class_id)))
+        # FIXME: not sure about the else section
+        '''
         if self.mode == 'eval':
             mask_label = ma.getmaskarray(ma.masked_equal(label, np.array(255)))
         else:
             mask_label = ma.getmaskarray(ma.masked_equal(label, np.array([255, 255, 255])))[:, :, 0]
+        '''
         
         mask = mask_label * mask_depth
 
         if self.add_noise:
             img = self.trancolor(img)
 
-        img = np.array(img)[:, :, :3]
+        img = np.array(img)[:, :, :3] # remove alpha channel
         img = np.transpose(img, (2, 0, 1))
         img_masked = img
 
-        if self.mode == 'eval':
-            rmin, rmax, cmin, cmax = get_bbox(mask_to_bbox(mask_label))
-        else:
-            rmin, rmax, cmin, cmax = get_bbox(meta['obj_bb'])
+        bbox = meta["objects"][0]["bounding_box"]
+        rmin, rmax, cmin, cmax = int(bbox["top_left"][0]), int(bbox["bottom_right"][0]), int(bbox["top_left"][1]), int(bbox["bottom_right"][1])
 
         img_masked = img_masked[:, rmin:rmax, cmin:cmax]
         #p_img = np.transpose(img_masked, (1, 2, 0))
-        #scipy.misc.imsave('evaluation_result/{0}_input.png'.format(index), p_img)
+        #Image._show(Image.fromarray(p_img))
 
-        target_r = np.resize(np.array(meta['cam_R_m2c']), (3, 3))
-        target_t = np.array(meta['cam_t_m2c'])
-        add_t = np.array([random.uniform(-self.noise_trans, self.noise_trans) for i in range(3)])
 
+        # the indices that is non-zero
         choose = mask[rmin:rmax, cmin:cmax].flatten().nonzero()[0]
         if len(choose) == 0:
             cc = torch.LongTensor([0])
@@ -173,45 +139,37 @@ class PoseDataset(data.Dataset):
         pt0 = (ymap_masked - self.cam_cx) * pt2 / self.cam_fx
         pt1 = (xmap_masked - self.cam_cy) * pt2 / self.cam_fy
         cloud = np.concatenate((pt0, pt1, pt2), axis=1)
-        cloud = cloud / 1000.0
+        cloud = cloud
 
-        if self.add_noise:
-            cloud = np.add(cloud, add_t)
+        #model_points = ply_vtx(model_path)
+        model_points = o3d.io.read_triangle_mesh(model_path)
+        model_points = np.array(model_points.vertices)
 
-        #fw = open('evaluation_result/{0}_cld.xyz'.format(index), 'w')
-        #for it in cloud:
-        #    fw.write('{0} {1} {2}\n'.format(it[0], it[1], it[2]))
-        #fw.close()
-
-        model_points = self.pt[obj] / 1000.0
         dellist = [j for j in range(0, len(model_points))]
         dellist = random.sample(dellist, len(model_points) - self.num_pt_mesh_small)
         model_points = np.delete(model_points, dellist, axis=0)
 
-        #fw = open('evaluation_result/{0}_model_points.xyz'.format(index), 'w')
-        #for it in model_points:
-        #    fw.write('{0} {1} {2}\n'.format(it[0], it[1], it[2]))
-        #fw.close()
+        target_transform = np.array(meta['objects'][0]['pose_transform'])
+        target_rotation = target_transform[:3, :3]
+        p = np.array([[ 0, 0, 1],
+                      [ 1, 0, 0],
+                      [ 0,-1, 0]])
+        real_target_rotation = np.matmul(target_rotation.T, p)
+        target_translation = target_transform[3,:3]
 
-        target = np.dot(model_points, target_r.T)
-        if self.add_noise:
-            target = np.add(target, target_t / 1000.0 + add_t)
-            out_t = target_t / 1000.0 + add_t
-        else:
-            target = np.add(target, target_t / 1000.0)
-            out_t = target_t / 1000.0
+        fixed_transform = np.array(object_data['exported_objects'][0]['fixed_model_transform'])
+        fixed_rotation = fixed_transform[:3, :3].T.astype(np.float64) / 100
+        fixed_translation = fixed_transform[3,:3].astype(np.float64) / 100
 
-        #fw = open('evaluation_result/{0}_tar.xyz'.format(index), 'w')
-        #for it in target:
-        #    fw.write('{0} {1} {2}\n'.format(it[0], it[1], it[2]))
-        #fw.close()
+        target = np.dot(model_points, fixed_rotation.T)#+ fixed_translation
+        target = np.dot(target, real_target_rotation.T) + target_translation / 100
 
         return torch.from_numpy(cloud.astype(np.float32)), \
                torch.LongTensor(choose.astype(np.int32)), \
                self.norm(torch.from_numpy(img_masked.astype(np.float32))), \
                torch.from_numpy(target.astype(np.float32)), \
                torch.from_numpy(model_points.astype(np.float32)), \
-               torch.LongTensor([self.objlist.index(obj)])
+               torch.LongTensor([object_id])
 
     def __len__(self):
         return self.length
@@ -225,86 +183,3 @@ class PoseDataset(data.Dataset):
         else:
             return self.num_pt_mesh_small
 
-
-
-border_list = [-1, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600, 640, 680]
-img_width = 480
-img_length = 640
-
-
-def mask_to_bbox(mask):
-    mask = mask.astype(np.uint8)
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
-    x = 0
-    y = 0
-    w = 0
-    h = 0
-    for contour in contours:
-        tmp_x, tmp_y, tmp_w, tmp_h = cv2.boundingRect(contour)
-        if tmp_w * tmp_h > w * h:
-            x = tmp_x
-            y = tmp_y
-            w = tmp_w
-            h = tmp_h
-    return [x, y, w, h]
-
-
-def get_bbox(bbox):
-    bbx = [bbox[1], bbox[1] + bbox[3], bbox[0], bbox[0] + bbox[2]]
-    if bbx[0] < 0:
-        bbx[0] = 0
-    if bbx[1] >= 480:
-        bbx[1] = 479
-    if bbx[2] < 0:
-        bbx[2] = 0
-    if bbx[3] >= 640:
-        bbx[3] = 639                
-    rmin, rmax, cmin, cmax = bbx[0], bbx[1], bbx[2], bbx[3]
-    r_b = rmax - rmin
-    for tt in range(len(border_list)):
-        if r_b > border_list[tt] and r_b < border_list[tt + 1]:
-            r_b = border_list[tt + 1]
-            break
-    c_b = cmax - cmin
-    for tt in range(len(border_list)):
-        if c_b > border_list[tt] and c_b < border_list[tt + 1]:
-            c_b = border_list[tt + 1]
-            break
-    center = [int((rmin + rmax) / 2), int((cmin + cmax) / 2)]
-    rmin = center[0] - int(r_b / 2)
-    rmax = center[0] + int(r_b / 2)
-    cmin = center[1] - int(c_b / 2)
-    cmax = center[1] + int(c_b / 2)
-    if rmin < 0:
-        delt = -rmin
-        rmin = 0
-        rmax += delt
-    if cmin < 0:
-        delt = -cmin
-        cmin = 0
-        cmax += delt
-    if rmax > 480:
-        delt = rmax - 480
-        rmax = 480
-        rmin -= delt
-    if cmax > 640:
-        delt = cmax - 640
-        cmax = 640
-        cmin -= delt
-    return rmin, rmax, cmin, cmax
-
-
-def ply_vtx(path):
-    f = open(path)
-    assert f.readline().strip() == "ply"
-    f.readline()
-    f.readline()
-    N = int(f.readline().split()[-1])
-    while f.readline().strip() != "end_header":
-        continue
-    pts = []
-    for _ in range(N):
-        pts.append(np.float32(f.readline().split()[:3]))
-    return np.array(pts)
--- experiments/scripts/train_custom.sh
+++ experiments/scripts/train_custom.sh
@@ -7,4 +7,4 @@ export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
 python3 ./tools/train.py --dataset custom\
-  --dataset_root ./datasets/custom/data
+  --dataset_root ./datasets/custom/custom_preprocessed
