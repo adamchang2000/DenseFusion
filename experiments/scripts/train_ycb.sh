c258641eb7394a7f8512c2784e132aac9134132c
sethzhao506@berkeley.edu <adamchang2000@berkeley.edu>
2022-02-01 13:46:30 -0800

    train ycb

--- experiments/scripts/train_ycb.sh
+++ experiments/scripts/train_ycb.sh
@@ -8,5 +8,6 @@ export CUDA_VISIBLE_DEVICES=0
 
 python ./tools/train.py --dataset ycb\
   --dataset_root ./datasets/ycb/YCB_Video_Dataset\
-  --workers 16\
-  --batch_size 16
+  --workers 8\
+  --batch_size 32\
+  --profile
